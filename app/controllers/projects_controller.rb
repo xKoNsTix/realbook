6117664d70e2301780e64257849a7868d92f39d8
Denis Stjepanovic <stjepanovic.dns@gmail.com>
2023-12-11 14:36:46 +0100

    expanded project controller actions - new & edit. projects can now be created with form. link to all projects added to navbar

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,8 +1,39 @@
 class ProjectsController < ApplicationController
+
     def index
         @projects = Project.all
     end
+
     def show
-        @project = Project.find_by(params[:id])
+        @project = Project.find(params[:id])
+    end
+
+    def new
+        @project = Project.new
+    end
+
+    def create
+        @project = Project.create(project_params)
+        if @project.save
+            redirect_to project_path(@project), notice: 'Event successfully created!'
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    # @event = Event.create(event_params)
+    # if @event.save
+    #   redirect_to event_path(@event), notice: 'Event successfully created!'
+    # else
+    #   render :new, status: :unprocessable_entity
+    # end
+
+    # def event_params
+    #     params.require(:event).permit(:name, :description, :location, :price, :starts_at, :capacity, :image_file_name, category_ids: [])
+    #   end
+    private
+
+    def project_params
+        params.require(:project).permit(:adress, :date, :video, :drone, :photo, :size)
     end
 end
